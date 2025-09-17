41ed7cbb688e060eb65f28ef6cd2086804f6f085
muscularbird <etienne.kretz@protonmail.com>
2025-09-17 16:25:29 +0200

    [+] guestack ui for tailwind styling and ready to use

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Tabs } from 'expo-router';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="workflows"
+        options={{
+          title: 'workflows',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="check" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
