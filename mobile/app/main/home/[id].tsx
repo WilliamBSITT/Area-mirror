1574e37a783329b1b4944ad3b6e3d7f9f2e58b8e
naouel <naouel.bouhali@epitech.eu>
2025-10-13 12:07:06 +0200

    [Fix] Design + [ADD] Header and connection butoons

--- mobile/app/main/home/[id].tsx
+++ mobile/app/main/home/[id].tsx
@@ -3,14 +3,21 @@ import React, { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { service } from ".";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from "expo-router";
+import { useRouter, router } from "expo-router";
 import api from "@/utils/api";
+import * as WebBrowser from 'expo-web-browser';
 
 export default function ServiceScreen() {
+  const handlePress = () => {
+    if (data?.auth_url) {
+      WebBrowser.openBrowserAsync(data?.auth_url);
+    } else {
+      console.warn('URL is missing');
+    }
+  };
   const { id } = useLocalSearchParams();
   const [data, setData] = useState<service | null >(null);
   const [icon, setIcon] = useState<string | null>(null)
-
   useEffect(() => {
 
     const fetchServices = async () => {
@@ -35,7 +42,7 @@ export default function ServiceScreen() {
 
   return (
     <View className="bg-[#F4FBFB] rounded-2xl flex h-full w-full">
-          <Text className="text-black font-bold ml-4 mt-10 justify-start">{data?.name}</Text>
+          <Text className="text-black text-4xl font-bold ml-4 mt-14 justify-start">{data?.name}</Text>
            <View className='items-center'>
                   <Image
                     source={{uri: `data:image/png;base64,${icon}`}}
@@ -43,10 +50,17 @@ export default function ServiceScreen() {
                     resizeMode="contain"
                   />
                 </View>
-          <Text className="text-black font-bold ml-4 mt-2 justify-start"> {data?.description}</Text>
+          <Text className="text-black text-2xl font-bold ml-4 mt-2 justify-start"> {data?.description}</Text>
           <Pressable className="absolute bottom-40 right-14 bg-blue-900 w-16 h-16 rounded-full" onPress={() => router.push('/main/workflows/newWorkflow')}>
                   <Image source={require("../../../images/plus-white.png")} className="w-10 h-10 m-auto"/>
           </Pressable>
+          {data?.auth_url != null && (
+          <Pressable
+            className="absolute bottom-40 left-9 bg-blue-900 w-36 h-16 rounded-full items-center justify-center"
+            onPress={handlePress}
+          >
+          <Text className="text-white text-sm">Connexion</Text>
+          </Pressable>)}
     </View>
   );
 }
--- mobile/app/main/home/index.tsx
+++ mobile/app/main/home/index.tsx
@@ -1,35 +1,58 @@
 
 import React, { useEffect, useState } from 'react';
-import { Text, Image, View, ScrollView } from 'react-native';
+import { Text, Image, View, ScrollView, Pressable, StyleSheet } from 'react-native';
 import {Link} from 'expo-router'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '@/utils/api';
 
+const Header: React.FC = () => {
+  return (
+    <View style={styles.header}>
+      <Image
+        source={require('../../../images/logo.png')}
+        style={{ width: 200, height: 120, marginLeft: 5, marginTop: 5 }}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+});
+
 function Card({name, path, id}: {name:string, path:any, id:number} ) {
   return (
-    <View className="bg-[#F4FBFB] w-70 h-70 rounded-2xl flex">
-      <View className="items-start">
-      <Text className="text-black font-bold ml-4 mt-2 justify-start">{name}</Text>
-      </View>
-      <View className='items-center'>
-        <Image
-          source={{uri: path}}
-          className="w-40 h-40"
-          resizeMode="contain"
-        />
-      </View>
-      <View className="items-end">
-      <Link
-        href={{
-          pathname: '/main/home/[id]',
-          params: { id: name },
-        }}
-        className='bg-slate-400 rounded-2xl p-1 w-1/3 justify-end flex text-center mr-2 mb-2'
-      >
-        Details
-      </Link>
-      </View>
-    </View>
+    <Link
+      href={{
+        pathname: '/main/home/[id]',
+        params: { id: name },
+      }}
+    asChild>
+      <Pressable className="bg-[#F4FBFB] w-70 h-70 rounded-2xl flex">
+        <View className="items-start">
+          <Text className="text-black font-bold ml-4 mt-2 justify-start">
+            {name}
+          </Text>
+        </View>
+        <View className="items-center">
+          <Image
+            source={{ uri: path }}
+            className="w-40 h-40"
+            resizeMode="contain"
+          />
+        </View>
+        <View className="items-end">
+          <Text className="bg-slate-400 rounded-2xl p-1 text-center mr-2 mb-2 text-white">
+            Details
+          </Text>
+        </View>
+      </Pressable>
+    </Link>
   );
 }
 
@@ -39,6 +62,7 @@ export interface service {
     image: string
     required: boolean
     description: string
+    auth_url: string
   }
 
 export default function Home() {
@@ -66,24 +90,25 @@ export default function Home() {
     fetchServices();
   }, []);
   return (
-    <ScrollView>
-    <View className="bg-white flex-1 ">
-      <Image
-        source={require('../../../images/logo.png')}
-        className="w-[200px] h-[120px] ml-5 mt-5"
-        resizeMode="contain"
-      />
-      
+  <View style={{ flex: 1 }}>
+    <Header />
+    <ScrollView
+      className="flex-1"
+      contentContainerStyle={{ paddingBottom: 40 }}
+    >
       <View className="flex flex-row flex-wrap mt-10 ml-4">
-        {
-          data.map((service: service)=>  
+        {data.map((service: service) => (
           <View className="w-1/2 p-5" key={service.name}>
-            <Card name={service.name} path={`data:image/png;base64,${service.image}`} id={service.id} key={service.name}/>
+            <Card
+              name={service.name}
+              path={`data:image/png;base64,${service.image}`}
+              id={service.id}
+            />
           </View>
-          )
-        }
+        ))}
       </View>
-    </View>
+      <View className='h-20'></View>
     </ScrollView>
-  );
+  </View>
+);
 }
