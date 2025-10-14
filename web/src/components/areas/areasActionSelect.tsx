163b3fca3dbdec2a4ab0e3218dddfda98b825db5
Isshin <julien.michel1026@icloud.com>
2025-10-14 14:05:25 +0200

    [+] add first test for select area for creation

---
+++ web/src/components/areas/areasActionSelect.tsx
@@ -0,0 +1,68 @@
+import * as React from "react"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+export default function AreasActionSelect() {
+    return (
+        <section className="grid md:grid-cols-3">
+            <div>
+                <div className="px-30">
+                    <h1 className="font-bold text-4xl">
+                        If
+                    </h1>
+                    <br />
+                    <br />
+                    <br />
+                </div>
+            </div>
+            <div>
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Fruits</SelectLabel>
+                            <SelectItem value="apple">Apple</SelectItem>
+                            <SelectItem value="banana">Banana</SelectItem>
+                            <SelectItem value="blueberry">Blueberry</SelectItem>
+                            <SelectItem value="grapes">Grapes</SelectItem>
+                            <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <br />
+                <br />
+                <br />
+            </div>
+            <div>
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Fruits</SelectLabel>
+                            <SelectItem value="apple">Apple</SelectItem>
+                            <SelectItem value="banana">Banana</SelectItem>
+                            <SelectItem value="blueberry">Blueberry</SelectItem>
+                            <SelectItem value="grapes">Grapes</SelectItem>
+                            <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <br />
+                <br />
+                <br />
+            </div>
+        </section>
+    );
+}
--- web/src/components/areas/areasCreation.tsx
+++ web/src/components/areas/areasCreation.tsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -12,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {PlusIcon} from "lucide-react";
+
 
 export default function AreasCreationDialog() {
     return (
@@ -35,10 +37,9 @@ export default function AreasCreationDialog() {
                             <Label htmlFor="name-1">Name</Label>
                             <Input id="name-1" name="name" />
                         </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="username-1">Username</Label>
-                            <Input id="username-1" name="username" />
-                        </div>
+                    </div>
+                    <div>
+
                     </div>
                     <DialogFooter>
                         <DialogClose asChild>
